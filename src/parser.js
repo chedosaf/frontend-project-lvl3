4186b260c228206a2ac8a732c38c79e9b3148cb2
Sasha Chadaev <chedosaf@mail.ru>
2021-07-22 16:34:43 +0300

    1st-1

---
+++ src/parser.js
@@ -0,0 +1,26 @@
+export default (xmlData) => {
+    const parser = new DOMParser();
+    const text = parser.parseFromString(xmlData, 'text/xml');
+    const feedTitle = text.querySelector('title');
+    const title = feedTitle.textContent;
+    const feedDescription = text.querySelector('description');
+    const description = feedDescription.textContent;
+    const extractPost = (post) => {
+      const titleNode = post.querySelector('title');
+      const linkNode = post.querySelector('link');
+      return {
+        title: titleNode.textContent,
+        link: linkNode.textContent,
+      };
+    };
+  
+    const postsNodes = text.querySelectorAll('item');
+    const posts = [...postsNodes].map(extractPost);
+    const result = {
+      title,
+      description,
+      posts,
+    };
+  
+    return result;
+  };
