b354980010926fbe302393728dd07ac0d5f48710
Sasha Chadaev <chedosaf@mail.ru>
2021-08-04 18:37:25 +0300

    update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const elements = {
   form: document.getElementsByClassName('rss-form'),
 };
 
+console.log(elements.form);
+
 elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(e.target);
