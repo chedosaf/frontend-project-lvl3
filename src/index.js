f423e9e6fbb5c3a26561f69250500780dec27d9f
Sasha Chadaev <chedosaf@mail.ru>
2021-08-04 18:35:32 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import '../template.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const elements = {
-  input: document.getElementById('url-input'),
-  button: document.getElementById('submitButton'),
   form: document.getElementsByClassName('rss-form'),
 };
 
