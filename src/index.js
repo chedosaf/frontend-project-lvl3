9167ef69fb077cf1bfd5213d77d31039ea916325
Sasha Chadaev <chedosaf@mail.ru>
2021-08-04 18:25:33 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import '../template.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-  const elements = {
-    input: document.getElementById('url-input'),
-    button: document.getElementById('submitButton'),
-    form: document.getElementById('form'),
-  };
+const elements = {
+  input: document.getElementById('url-input'),
+  button: document.getElementById('submitButton'),
+  form: document.getElementById('form'),
+};
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e.target);
+});
+
