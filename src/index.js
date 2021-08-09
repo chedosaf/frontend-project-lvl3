e49d4cb13f476a8221138cc550b97a40c77b7093
Sasha Chadaev <chedosaf@mail.ru>
2021-08-09 19:00:38 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import '../template.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import parser from '../parser.js';
 
+const state = {
+    form: {
+      fields: {
+        url: '',
+      },
+      processState: 'filling',
+      valid: false,
+      errors: {},
+    },
+    feeds: [],
+    posts: [],
+  };
 
 const forms = document.getElementsByClassName('rss-form');
 
@@ -10,9 +23,21 @@ const form = forms[0];
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target)
-    console.log(formData.get('url'));
-  });
+    const formData = new FormData(e.target);
+    
+    // console.log(formData.get('url'));
+//     const { url } = state.form.fields;
+//     fetch(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent('https://wikipedia.org')}`)
+//   .then
+
+  fetch(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(formData.get('url'))}`)
+    .then((resp) => {
+        const parsedData = parse(resp.data);
+        state.posts.push(parsedData.posts);
+        console.log(state.posts);
+    });
+      
+});
 
 
 
