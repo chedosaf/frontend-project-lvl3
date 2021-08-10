30dcee1f02a692edbedec644ccc49770d1367c54
Sasha Chadaev <chedosaf@mail.ru>
2021-08-10 16:22:58 +0300

    update airbnb

--- src/index.js
+++ src/index.js
@@ -1,57 +1,46 @@
-import '../template.css'; 
+import '../template.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import parse from './parser';
 
 const state = {
-    form: {
-      fields: {
-        url: '',
-      },
-      processState: 'filling',
-      valid: false,
-      errors: {},
+  form: {
+    fields: {
+      url: '',
     },
-    feeds: [],
-    posts: [],
-  };
+    processState: 'filling',
+    valid: false,
+    errors: {},
+  },
+  feeds: [],
+  posts: [],
+};
 
 const forms = document.getElementsByClassName('rss-form');
 
 const form = forms[0];
 
-
 const addIdToPost = (id) => (post) => ({ ...post, feedId: id });
 
 const makeFeed = (url, parsedData) => ({
-    id: url,
-    title: parsedData.title,
-    description: parsedData.description,
-  });
-  
+  id: url,
+  title: parsedData.title,
+  description: parsedData.description,
+});
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    
-    // console.log(formData.get('url'));
-//     const { url } = state.form.fields;
-//     fetch(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent('https://wikipedia.org')}`)
-//   .then
-
+  e.preventDefault();
+  const formData = new FormData(e.target);
   fetch(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(formData.get('url'))}`)
-  .then(response => {
-    if (response.ok) return response.json()
-    throw new Error('Network response was not ok.')
-  })
-  .then((data) => {
+    .then((response) => {
+      if (response.ok) return response.json();
+      throw new Error('Network response was not ok.');
+    }).then((data) => {
       const parsedData = parse(data.contents);
       const feed = makeFeed(formData.get('url'), parsedData);
       state.feeds.push(feed);
       const feedPosts = parsedData.posts.map(addIdToPost(feed.id));
-      console.log(feedPosts);
-  });
-      
+      state.posts.push(...feedPosts);
+      console.log(state.posts);
+      console.log(state.feeds);
+    });
 });
-
-
-
