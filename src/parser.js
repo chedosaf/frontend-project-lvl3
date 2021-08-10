1da1e9d951e9060359d21a19c87810b0b90e878a
Sasha Chadaev <chedosaf@mail.ru>
2021-08-10 16:33:54 +0300

    update

--- src/parser.js
+++ src/parser.js
@@ -1,24 +1,25 @@
 export default (xmlData) => {
-    const parser = new DOMParser();
-    const text = parser.parseFromString(xmlData, 'text/xml');
-    const feedTitle = text.querySelector('title');
-    const title = feedTitle.textContent;
-    const feedDescription = text.querySelector('description');
-    const description = feedDescription.textContent;
-    const postsNodes = text.querySelectorAll('item');
-    const posts = [...postsNodes].map((post) => {
-      const titleNode = post.querySelector('title');
-      const linkNode = post.querySelector('link');
-      return {
-        title: titleNode.textContent,
-        link: linkNode.textContent,
-      };
-    });
-    const result = {
-      title,
-      description,
-      posts,
+  const parser = new DOMParser();
+  const text = parser.parseFromString(xmlData, 'text/xml');
+  const feedTitle = text.querySelector('title');
+  const title = feedTitle.textContent;
+  const feedDescription = text.querySelector('description');
+  const description = feedDescription.textContent;
+  const postsNodes = text.querySelectorAll('item');
+  const posts = [...postsNodes].map((post) => {
+    const titleNode = post.querySelector('title');
+    const linkNode = post.querySelector('link');
+    const descriptionNode = post.querySelector('description');
+    return {
+      title: titleNode.textContent,
+      link: linkNode.textContent,
+      description: descriptionNode.textContent,
     };
-  
-    return result;
+  });
+  const result = {
+    title,
+    description,
+    posts,
   };
+  return result;
+};
