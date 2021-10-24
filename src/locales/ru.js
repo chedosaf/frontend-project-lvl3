42a73dcdfac938dc7037474110cf6e3cf30b546f
Sasha Chadaev <chedosaf@mail.ru>
2021-10-24 23:09:07 +0300

    edit RSS уже существует

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -2,7 +2,7 @@ export default {
   translation: {
     feedBack: 'RSS успешно загружен',
     required: 'Поле должно быть заполнено',
-    url: 'URL должен быть рабочим',
+    url: 'Ссылка должна быть валидным URL',
     notOneOf: 'RSS уже существует',
     parseError: 'Ресурс не содержит валидный RSS',
     show: 'Просмотр',
